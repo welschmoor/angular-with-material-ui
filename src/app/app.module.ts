f58f295e0fee9048dd591c1a5a87ffccbaaf7c6e
Johannes Welsch <welschmoor@gmail.com>
2022-03-31 23:01:52 +0200

    added toolbar, added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import { LayoutModule } from './layout/layout.module';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
